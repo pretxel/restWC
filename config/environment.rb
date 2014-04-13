b75988ce82c3cb05ad37a453d4c183ab123ef9d9
Edsel Serrano <pretxel@hotmail.com>
2014-04-13 00:12:42 -0500

    Cambio RAILS_ENV

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-RAILS_ENV = 'production'
 
 # Initialize the Rails application.
 SwaggerDocsSample::Application.initialize!
